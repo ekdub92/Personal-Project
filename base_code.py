787c961140e3d6a5b50c3c9701c1eb1a3cf75b1c
ekdub92 <42200769+ekdub92@users.noreply.github.com>
2026-01-11 00:35:53 +0900

    Update base_code.py

--- base_code.py
+++ base_code.py
@@ -1,26 +1,26 @@
 from langchain_chroma import Chroma
+from langchain_community.document_loaders.json_loader import JSONLoader
 from langchain_core.documents import Document
 from langchain_core.embeddings import FakeEmbeddings
+from langchain_text_splitters import RecursiveJsonSplitter 
 
-documents = [
-    Document(
-        page_content="?",
-        metadata={"category": "?"}
-    ),
-    Document(
-        page_content="!",
-        metadata={"category": "!"}
-    )
-]
+json_data = JSONLoader(
+    file_path="ebook_1022.jsonl",
+    jq_schema=".",
+    text_content=False,
+    json_lines=True
+).load() # HuggingFace : opendatalab / ScienceMetaBench
+
+splitter = RecursiveJsonSplitter(max_chunk_size=300)
 
 vector_store = Chroma.from_documents(
-    documents=documents,
+    documents=json_data,
     embedding=FakeEmbeddings(size=1352),
     persist_directory="./test_db"
 )
 
 query = "?"
-result = vector_store.search_similarity(query)
-for i, document in result:
-    print(f"{i+1}번쨰 문서 : {document.page_content}")
+result = vector_store.similarity_search(query)
+for i, document in enumerate(result):
+    print(f"{i+1}번째 문서 : {document.page_content}")
     print(f"출처 : {document.metadata}")
