f8bd5efbc95e79083c45185d8b776fb7f30d31ff
ekdub92 <42200769+ekdub92@users.noreply.github.com>
2026-01-11 19:28:31 +0900

    Update base_mnist_code.py

--- base_mnist_code.py
+++ base_mnist_code.py
@@ -4,8 +4,11 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 
 transform = transforms.Compose([transforms.ToTensor()])
-train_dataset = datasets.MNIST(root='.', download=True, transform=transform)
+train_dataset = datasets.MNIST(root='.', train=True, download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+eval_dataset = datasets.MNIST(root='.', train=False, download=True, transform=transform)
+eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=32, shuffle=False)
 
 class SimpleNN(nn.Module):
     def __init__(self):
@@ -23,11 +26,23 @@ class SimpleNN(nn.Module):
 model = SimpleNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+epochs = 1
 
-for epoch in range(5):
-    for images, labels in train_loader:
+model.train()
+for epoch in range(epochs):
+    for (images, labels) in train_loader:
         outputs = model(images)
         loss = criterion(outputs, labels)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+model.eval()
+correct, total = 0, 0
+for (images, labels) in eval_loader:
+    outputs = model(images)
+    probs, indices = torch.max(outputs, 1)
+    total += outputs.size(0)
+    correct += (indices == labels).sum()
+    print(f"total: {total}, correct: {correct}")
+    break
